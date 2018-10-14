0830d8c12be4897bc7d65ab5b4cf0df12411f218
Arun Motoori <arun.motoori@gmail.com>
2018-10-15 00:00:50 +0530

    Adding project code to GitHub repo

---
+++ src/test/java/pfPack/tests/LoginTest.java
@@ -0,0 +1,62 @@
+package pfPack.tests;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.LogStatus;
+
+import pfPack.pages.LaunchPage;
+import pfPack.tests.base.BaseTest;
+import pfPack.util.Constants;
+
+//This is a LoginTest
+public class LoginTest extends BaseTest {
+	
+	//This is a testLogin method
+	@Test
+	public void testLogin() {
+		
+		 eTest = eReport.startTest("LoginTest");
+		 
+		 eTest.log(LogStatus.INFO,"Login Test has started");
+		 
+		 openBrowser(Constants.BROWSER_TYPE);
+		 
+		 LaunchPage launchPage = new LaunchPage(driver,eTest);
+		 PageFactory.initElements(driver,launchPage);
+		 
+		 boolean loginStatus = launchPage.goToLoginPage();
+		 
+		 if(loginStatus) {
+			 
+			reportPass("Login test passed");
+			 
+		 }else {
+			 
+			 reportFail("Login Test failed");
+			 
+		 }
+		 
+	}
+	
+	@AfterMethod
+	public void testClosure() {
+		
+		if(eReport!=null) {
+			
+			eReport.endTest(eTest);
+			eReport.flush();
+			
+		}
+		
+		if(driver!=null) {
+			
+			driver.quit();
+			
+		}
+		
+		
+	}
+	
+
+}
